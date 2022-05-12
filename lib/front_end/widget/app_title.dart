ebc3427c743b6fbae85ceff8ea28f53e0c23b15d
UG_Shorty <86832133+UpayanGhosh@users.noreply.github.com>
2022-05-12 21:25:18 +0530

    Search Bar animation and Custom font implementation.
    Upayan

--- lib/front_end/widget/app_title.dart
+++ lib/front_end/widget/app_title.dart
@@ -1,24 +1,29 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 
 class AppTitle extends StatelessWidget {
   const AppTitle({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('Pixa', style: TextStyle(
-          fontSize: 40,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),),
-        Text('Mart', style: TextStyle(
-          fontSize: 40,
-          fontWeight: FontWeight.bold,
-          color: Colors.blue,
-        ),),
-      ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text('Pixa', style: TextStyle(
+            fontSize: 50,
+            fontFamily: 'Raunchies',
+            fontWeight: FontWeight.bold,
+          ),),
+          Text('Mart', style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+            fontFamily: 'Raunchies',
+          ),),
+        ],
+      ),
     );
   }
 }
