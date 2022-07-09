b73ba73e786afedd8598294fa0e591aeab903184
Inst1nct007 <94550979+Inst1nct007@users.noreply.github.com>
2022-07-09 09:56:59 +0530

    login page made responsive

--- lib/front_end/pages/login_page.dart
+++ lib/front_end/pages/login_page.dart
@@ -18,17 +18,15 @@ class LoginPage extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: Icon(Icons.arrow_back_ios, size: 20, color: Colors.white),
+          icon: Icon(Icons.arrow_back_ios, size: MediaQuery.of(context).size.height / 41.7, color: Colors.white),
         ),
       ),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
         child: Container(
-          color: Colors.black87,
           height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Expanded(
                 child: Column(
@@ -61,36 +59,42 @@ class LoginPage extends StatelessWidget {
                       padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 9.8),
                       child: Column(
                         children: [
-                          TextFormField(
-                            cursorColor: Colors.white,
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide:
-                                      BorderSide(width: 3, color: Colors.white),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                        width: 3, color: Colors.white)),
-                                labelText: 'Email',
-                                labelStyle: TextStyle(color: Colors.white, fontFamily: 'Nexa', fontWeight: FontWeight.bold)),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 13.9,
+                            child: TextFormField(
+                              cursorColor: Colors.white,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(width: 3, color: Colors.white),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          width: 3, color: Colors.white)),
+                                  labelText: 'Email',
+                                  labelStyle: TextStyle(color: Colors.white, fontFamily: 'Nexa', fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.height / 52.125)),
+                            ),
                           ),
                           SizedBox(
                             height: MediaQuery.of(context).size.height / 27.8,
                           ),
-                          TextFormField(
-                            cursorColor: Colors.white,
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide:
-                                      BorderSide(width: 3, color: Colors.white),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                        width: 3, color: Colors.white)),
-                                labelText: 'Password',
-                                labelStyle: TextStyle(color: Colors.white, fontFamily: 'Nexa', fontWeight: FontWeight.bold)),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 13.9,
+                            child: TextFormField( // todo add code to obscure text
+                              cursorColor: Colors.white,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(width: 3, color: Colors.white),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          width: 3, color: Colors.white)),
+                                  labelText: 'Password',
+                                  labelStyle: TextStyle(color: Colors.white, fontFamily: 'Nexa', fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.height / 52.125)),
+                            ),
                           ),
                         ],
                       ),
@@ -112,7 +116,7 @@ class LoginPage extends StatelessWidget {
                         "Log In",
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                            fontSize: MediaQuery.of(context).size.height / 46.33,
                             color: Colors.white,
                             fontFamily: 'Nexa'),
                       ),
@@ -120,8 +124,7 @@ class LoginPage extends StatelessWidget {
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         side: BorderSide(color: Colors.transparent),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 90, vertical: 20),
+                        padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 4.35, vertical: MediaQuery.of(context).size.height / 41.7),
                         elevation: 0,
                         primary: Color(0xff63c54f),
                         shape: RoundedRectangleBorder(
@@ -134,7 +137,7 @@ class LoginPage extends StatelessWidget {
                         "Login With Google",
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                            fontSize: MediaQuery.of(context).size.height / 46.33,
                             color: Colors.white,
                             fontFamily: 'Nexa'),
                       ),
@@ -144,7 +147,7 @@ class LoginPage extends StatelessWidget {
                         Navigator.pushNamed(context, '/signUp');
                       },
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Text("Don't have an account?",
                               style: TextStyle(
@@ -153,12 +156,11 @@ class LoginPage extends StatelessWidget {
                                 color: Colors.white,
                               )),
                           Container(
-                            margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
                             child: Text(
                               "Sign up",
                               style: TextStyle(
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 18,
+                                  fontSize: MediaQuery.of(context).size.height / 46.33,
                                   color: Colors.white,
                                   fontFamily: 'Nexa'),
                             ),
@@ -171,41 +173,13 @@ class LoginPage extends StatelessWidget {
               ),
               Container(
                 height: MediaQuery.of(context).size.height / 3,
+                width: MediaQuery.of(context).size.width / 1.5,
                 child: Lottie.asset('assets/lottie/LoginPage.json'),
-              )
+              ),
             ],
           ),
         ),
       ),
-    );
-  }
-
-  Widget makeInput({label, obscureText = false}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: TextStyle(
-              fontSize: 15, fontWeight: FontWeight.w400, color: Colors.white),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        TextField(
-          obscureText: obscureText,
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            enabledBorder:
-                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-            border:
-                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-          ),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-      ],
     );
   }
 }
